b1e13662a33320e7e97cd522506f1dedcec97f88
Gernot Pointner <gernot.pointner@googlemail.com>
2016-10-05 21:44:03 +0200

    Enhance simple_test.sh

--- client/shell/simple_test.sh
+++ client/shell/simple_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Trying to access the backend on localhost"
-curl localhost:8081
+curl localhost:8081 > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Could not access localhost"
+    exit 1
+fi
+echo "Accessing has worked!"
