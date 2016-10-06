32b198ff10c258833de954c0345d2e004e18307f
Gernot Pointner <gernot.pointner@googlemail.com>
2016-10-06 20:39:31 +0200

    Only show bean names for log level DEBUG

--- backend/src/main/java/de/gernd/simplemon/Application.java
+++ backend/src/main/java/de/gernd/simplemon/Application.java
@@ -1,5 +1,7 @@
 package de.gernd.simplemon;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,13 +12,16 @@ public class Application {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(Application.class, args);
+        Logger logger = Logger.getLogger("Application");
 
-        // print out all bean names
-        System.out.println("Printing all bean names");
+        // print out all bean names if log level debug is set
+        if (logger.getEffectiveLevel().equals(Level.DEBUG)) {
+            logger.debug("Printing all bean names");
 
-        String[] beanNames = context.getBeanDefinitionNames();
-        for (String beanName : beanNames){
-            System.out.println(beanName);
+            String[] beanNames = context.getBeanDefinitionNames();
+            for (String beanName : beanNames) {
+                logger.debug(beanName);
+            }
         }
     }
 }
