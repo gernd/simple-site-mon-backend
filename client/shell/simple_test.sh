8d9ccdcd8279443715fbdb24adcd4fe63427b8bc
Gernot Pointner <gernot.pointner@googlemail.com>
2016-10-06 19:54:59 +0200

    Change simple test shell script to just display the output of curl

--- client/shell/simple_test.sh
+++ client/shell/simple_test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 echo "Trying to access the backend on localhost"
-curl localhost:8081 > /dev/null
-if [ $? -ne 0 ]; then
-    echo "Could not access localhost"
-    exit 1
-fi
-echo "Accessing has worked!"
+curl localhost:8081 
