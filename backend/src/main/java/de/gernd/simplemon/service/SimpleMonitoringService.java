83473ad1b96a58d8c2faf2c30fa01e399be3a0b1
Gernot Pointner <gernot.pointner@googlemail.com>
2016-10-25 07:01:33 +0200

    Adds simple monitoring functionality

--- backend/src/main/java/de/gernd/simplemon/service/SimpleMonitoringService.java
+++ backend/src/main/java/de/gernd/simplemon/service/SimpleMonitoringService.java
@@ -1,10 +1,17 @@
 package de.gernd.simplemon.service;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Implements the simple website monitoring functionality
@@ -14,15 +21,28 @@ public class SimpleMonitoringService {
 
     private final Logger log = Logger.getLogger(SimpleMonitoringService.class);
     private final List<String> monitoredSites = new LinkedList<>();
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
     /**
      * Start monitoring a website
      *
      * @param url URL of the website to monitor
      */
-    public void startMonitoring(String url) {
+    public synchronized void startMonitoring(String url) {
         log.info("Request to start monitoring " + url);
         monitoredSites.add(url);
+        final Runnable monitoringTask = () ->  {
+            System.out.println("Monitoring " + url);
+            // TODO add Rest Template
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+            if(response.getStatusCode().equals(HttpStatus.OK)){
+                System.out.println("Received 200");
+            } else {
+                System.out.println("Received status code " + response.getStatusCode().getReasonPhrase());
+            }
+        };
+        executorService.scheduleAtFixedRate(monitoringTask, 5, 5, TimeUnit.SECONDS);
     }
 
     public List<String> getMonitoredSites() {
