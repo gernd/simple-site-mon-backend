3c41ece544da3fe0bd7c2a41b408e8825f8ad1bd
Gernot Pointner <gernot.pointner@googlemail.com>
2017-06-09 07:04:42 +0200

    Implement fetching of monitored resources

--- webapp/monitoring.js
+++ webapp/monitoring.js
@@ -1,5 +1,19 @@
+var updatedMonitoredResources = function(){
+    console.log("Updating monitored resources");
+    $.get("http://localhost:8081/monitored-sites",function(response){
+        console.log(response);
+        var html = "";
+        for(monitoredResource of response.monitoredSites){
+            html += monitoredResource.url + '<br>';
+        }
+        console.log(html);
+        $('#monitoredResources').html(html);
+    });
+};
+
 $(document).ready(function(){
     console.log("Ready");
+    updatedMonitoredResources();
 
     $("#addUrlButton").click(function(){
         var url = $("#urlInput").val()
@@ -13,8 +27,8 @@ $(document).ready(function(){
           dataType:"json",
           complete: function(){
             console.log("Added");
+            updatedMonitoredResources();
           }
         });
-
     });
 })
